76b8b1c3b86695aecfc3929ae34f4715e187aac1
chenlianghong <clh021@gmail.com>
2022-03-28 23:08:31 +0800

    add start.sh

---
+++ start.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+# leehom Chen clh021@gmail.com
+# 本脚本旨在启动一个随处可运行的 vim 配置环境。将自己熟悉的配置带着走。
+set -e
+
+# 路径准备
+OldPath=`pwd`
+cd "$( dirname "${BASH_SOURCE[0]}" )"
+ProjPath=`pwd`
+
+# arch=`dpkg --print-architecture` # amd64
+arch=$(uname -m) # x86_64
+
+# 优先启用 neovim, 其次是 vim
+nvim=
